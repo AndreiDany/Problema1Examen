5d56799ac8a4a24961405d3c68b2ca6a526bf2d6
Daniel Leon Andrei <daniel.andrei@emmanuel.ro>
2022-02-07 09:36:27 +0200

    Problema1

--- Problema1Examen/Program.cs
+++ Problema1Examen/Program.cs
@@ -6,7 +6,27 @@ namespace Problema1Examen
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Introduceti numarul de elemente ale vectorului!");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] vector = new int[n];
+
+            Console.WriteLine("Introduceti elementele vectorului!");
+            for (int i = 0; i < vector.Length; i++)
+            {
+                Console.Write($"vector[{i}]= ");
+                vector[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine(SumaElementeVector(vector, n));
+        }
+
+        static int SumaElementeVector(int[] vector, int n)
+        {
+            if (n == 1)
+                return vector[0];
+            else
+                return vector[n - 1] + SumaElementeVector(vector, n - 1);
         }
     }
 }
