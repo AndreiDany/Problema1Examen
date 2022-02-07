41efb09d98cd582b5b2f7c204d148e081283d2bd
Daniel Leon Andrei <daniel.andrei@emmanuel.ro>
2022-02-07 09:24:58 +0200

    Hello

---
+++ Problema1Examen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Problema1Examen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
